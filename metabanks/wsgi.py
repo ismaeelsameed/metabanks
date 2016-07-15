964886fd754fb89baa34937b52076f1b0af3b128
ismaeel-jamalon <ismaeel@jamalon.com>
2016-07-15 17:51:43 +0300

    aws settings file

--- metabanks/wsgi.py
+++ metabanks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metabanks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metabanks.settings_aws")
 
 application = get_wsgi_application()
